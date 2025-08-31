529c3c9e6f2a4733ccf3ad79f5574ca5f1393238
prakrriti4097 <p.bhattarai.4097@westcliff.edu>
2025-08-31 09:26:57 +0545

    Update

--- src/q9.c
+++ src/q9.c
@@ -1,11 +1,9 @@
-
 #include <stdio.h>
 int main() {
     int totalDays, weeks, days;
-    printf("Enter number of days: ");
+    printf("Enter the number of days: ");
     scanf("%d", &totalDays);
     weeks = totalDays / 7;
     days = totalDays % 7;
-    printf("%d days is equivalent to %d week(s) and %d day(s)\n", totalDays, weeks, days);
     return 0;
 }
